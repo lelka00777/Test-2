f24ddb07e686086b40d9411b63b5242259e4ed41
lelka00777 <lelka00777@gmail.com>
2022-02-22 01:42:03 +0300

    22.02

---
+++ Grasshopper - Summation.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+var summation = function (num) {
+    let res = 0;
+    
+    for (i = 1; i <= num; i++)
+          res += i;
+       return res;
+  }
